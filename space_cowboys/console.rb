2cf71b59c28706513c839ec9dad80b5220009868
martabeveridge <marta@beveridge.uk>
2018-03-20 16:19:04 +0000

    file structure and object created

---
+++ space_cowboys/console.rb
@@ -0,0 +1,14 @@
+require('pry')
+require_relative('models/bounty')
+
+garrus_hash = { 'name' => 'Garrus Vakarian', 'species' => 'Turian', 'bounty_value' => 7000, 'homeworld' => 'Palaven', 'favourite_weapon' => 'Sniper Rifle'}
+
+bounty1 = Bounty.new( garrus_hash )
+
+wrex_hash = { 'name' => 'Urdnot Wrex', 'species' => 'Krogan', 'bounty_value' => 5000, 'homeworld' => 'Tuchanka', 'favourite_weapon' => 'Shotgun'}
+
+bounty2 = Bounty.new( wrex_hash )
+
+shepard_hash = { 'name' => 'Commander Shepard', 'species' => 'Human', 'bounty_value' => 10000, 'homeworld' => 'Earth', 'favourite_weapon' => 'Sub Machine Gun'}
+
+bounty3 = Bounty.new( shepard_hash )
---
+++ space_cowboys/models/bounty.rb
@@ -0,0 +1,97 @@
+require('pg')
+require('pry')
+
+class Bounty
+  attr_reader :name, :species, :bounty_value, :homeworld, :favourite_weapon
+
+  def initialize( options )
+    @name = options['name']
+    @species = options['species']
+    @bounty_value = options['bounty_value']
+    @homeworld = options ['homeworld']
+    @favourite_weapon = options ['favourite_weapon']
+  end
+
+  def save
+    # save all the info about this object in a DB
+    db = PG.connect( { dbname: 'pizza_shop', host: 'localhost'} )
+
+    pretty_sql = "
+    INSERT INTO pizza_orders
+      (name, color, pizza_type, quantity)
+      VALUES
+        ($1,$2,$3,$4)
+      ;
+    "
+    values = [@name, @color, @pizza_type, @quantity]
+
+    db.prepare("save_it", pretty_sql)
+    db.exec_prepared("save_it", values)
+
+    db.close()
+  end
+
+  def PizzaOrder.all
+      db = PG.connect( { dbname: "pizza_shop", host: "localhost" })
+
+      sql = "SELECT * FROM pizza_orders ORDER BY name;"
+      db.prepare("get all", sql)
+      orders = db.exec_prepared("get all")
+      # binding.pry
+
+      db.close()
+
+      pizza_order_objects = orders.map { |order_hash| PizzaOrder.new(order_hash) }
+
+      return pizza_order_objects
+  end
+
+
+  def PizzaOrder.delete_all
+      db = PG.connect( { dbname: "pizza_shop", host: "localhost" })
+
+      sql = "DELETE FROM pizza_orders;"
+
+      db.prepare("del all", sql)
+      orders = db.exec_prepared("del all")
+
+      db.close()
+  end
+
+  def update()
+    db = PG.connect( { dbname: "pizza_shop", host: "localhost" })
+
+    sql = "
+      UPDATE pizza_orders
+      SET (name, color, pizza_type, quantity)
+      = ($1,$2,$3,$4)
+      WHERE id = $5
+      ;
+    "
+    values = [@name, @color, @pizza_type, @quantity, @id]
+
+    db.prepare("update", sql)
+    db.exec_prepared("update", values)
+
+    db.close()
+  end
+
+
+
+  def delete()
+    db = PG.connect( { dbname: "pizza_shop", host: "localhost" })
+
+    sql = " DELETE FROM pizza_orders
+            WHERE id = $1
+      ;
+    "
+    values = [@id]
+
+    db.prepare("delete", sql)
+    db.exec_prepared("delete", values)
+
+    db.close()
+  end
+
+
+end
